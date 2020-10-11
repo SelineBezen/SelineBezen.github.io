a525a0d1a2a52e382344afb932585dda40fd69d5
Amir H. E <piano337@yahoo.de>
2020-10-11 14:04:21 +0200

    Better scripts

---
+++ prepScripts/scripts/00_checkDir.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# dpkg -s $1 &> /dev/null
+# 
+# if [ $? -eq 0 ]; then
+#     echo "Package  is installed!"
+# else
+#     echo "Package  is NOT installed!"
+# fi
+
+echo "Run 'sudo apt-get install imagemagick' before executing this script!"
+
+dir_repo="SelineBezen.github.io"
+dir_data="_data"
+dir_paintings="img/paintings/"
+dir_cur="$PWD"
+fil_csv="paintings.csv"
+
+[[ "$dir_cur" != *"$dir_repo"* ]] && echo "Go to '$dir_repo' repository dir!" && exit
+
+while [[ "${PWD##*/}" != "$dir_repo" ]]; do
+  cd ..
+done
+
+changeDirToDesired(){
+  [ "${PWD##*/}" == "$dir_repo" ] && cd $1
+}
